ca585c82a14cd7640755e71c944147901e1bcd41
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2024-02-22 12:03:44 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A proyect to optimize the time that takes you dress yourself',
+    author='Estban Berumen / Mariana Garcia',
+    license='MIT',
+)
